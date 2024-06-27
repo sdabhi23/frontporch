cd16b335e561b172b4a64bfceb7a97b43f9c697c
shrey <shrey.dabhi23@gmail.com>
2024-06-27 20:14:48 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
