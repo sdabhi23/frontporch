a9c501b4d7cab1e786045139808c9d2c597cbada
shrey <shrey.dabhi23@gmail.com>
2024-07-04 12:24:54 +0530

    try out cobra

--- backend/go.mod
+++ backend/go.mod
@@ -5,15 +5,18 @@ go 1.22.2
 require (
 	github.com/gorilla/handlers v1.5.2
 	github.com/shirou/gopsutil/v4 v4.24.5
+	github.com/spf13/cobra v1.8.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20240513124658-fba389f38bae // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tklauser/go-sysconf v0.3.14 // indirect
 	github.com/tklauser/numcpus v0.8.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
--- backend/main.go
+++ backend/main.go
@@ -1,58 +1,88 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/gorilla/handlers"
+	"github.com/spf13/cobra"
 
 	"frontporch/config"
 	"frontporch/sysinfo"
 )
 
+var appConfig = &config.Config{}
+
+func SetupCommands() *cobra.Command {
+	var configPath string
+	cmd := &cobra.Command{
+		Use:               "frontporch",
+		Short:             "FrontPorch is a server monitoring tool",
+		PersistentPreRunE: commandSetup(&configPath),
+		SilenceUsage:      true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fs := http.FileServer(http.Dir("../frontend/dist"))
+			http.Handle("/", fs)
+
+			http.HandleFunc("/api/sysinfo", func(w http.ResponseWriter, r *http.Request) {
+				var response []sysinfo.SysInfo
+
+				switch appConfig.Task {
+				case config.DaemonTask:
+					response = sysinfo.GetDaemonSystemInfo()
+				case config.ServerTask:
+					response = sysinfo.GetServerSystemInfo(&appConfig.Servers)
+				default:
+					response = []sysinfo.SysInfo{}
+				}
+
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(response)
+			})
+
+			http.HandleFunc("/api/config", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+				w.Header().Set("Content-Type", "application/json")
+				json.NewEncoder(w).Encode(*appConfig)
+			})
+
+			serverAddress := fmt.Sprintf("%s:%d", appConfig.HTTP.Host, appConfig.HTTP.Port)
+			fmt.Printf("Server is running at http://%s", serverAddress)
+			go func() {
+				if err := http.ListenAndServe(serverAddress, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux)); err != nil {
+					log.Fatal(err, "http server startup failed")
+				}
+			}()
+			<-cmd.Context().Done()
+			fmt.Println("http server shutdown initiated")
+			return nil
+		},
+	}
+	cmd.PersistentFlags().StringVar(&configPath, "config-file", "", "YAML file for config management")
+	return cmd
+}
+
+func commandSetup(configPath *string) func(*cobra.Command, []string) error {
+	return func(command *cobra.Command, strings []string) (err error) {
+		appConfig = config.ParseConfigFile(configPath)
+		return
+	}
+}
+
 func main() {
-	// parse all the flags passed to the program in main()
-	// use the flag package to parse the flags
-	appConfigFile := flag.String("config-file", "", "config file required to setup the server")
-	flag.Parse()
-
-	appConfig := config.ParseConfigFile(appConfigFile)
-
-	fs := http.FileServer(http.Dir("../frontend/dist"))
-	http.Handle("/", fs)
-
-	http.HandleFunc("/api/sysinfo", func(w http.ResponseWriter, r *http.Request) {
-		var response []sysinfo.SysInfo
-
-		switch appConfig.Task {
-			case config.DaemonTask:
-				response = sysinfo.GetDaemonSystemInfo()
-			case config.ServerTask:
-				response = sysinfo.GetServerSystemInfo(&appConfig.Servers)
-			default:
-				response = []sysinfo.SysInfo{}
-		}
-
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	})
-
-	http.HandleFunc("/api/config", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(*appConfig)
-	})
-
-	serverAddress := fmt.Sprintf("%s:%d", appConfig.HTTP.Host, appConfig.HTTP.Port)
-	fmt.Printf("Server is running at http://%s", serverAddress)
-	log.Fatal(http.ListenAndServe(serverAddress, handlers.LoggingHandler(os.Stdout, http.DefaultServeMux)))
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	cmd := SetupCommands()
+	if err := cmd.ExecuteContext(ctx); err != nil {
+		os.Exit(1)
+	}
 }
