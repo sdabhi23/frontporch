b79fb400f1965719578c682d0255e82192d134ef
shrey <shrey.dabhi23@gmail.com>
2024-07-19 03:37:51 +0530

    setup basic frontend skeleton

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,14 +11,25 @@ import { FaRotate, FaServer } from "react-icons/fa6";
 import { TbAppsFilled } from "react-icons/tb";
 
 import { useEffect, useState } from "react";
+import { getServerStatuses, getWidgets } from "../utils/apis.js";
 import { ServerStatus, ServerStatusSkeleton } from "./Server.jsx";
-
-import { getServerStatuses } from "../utils/apis.js";
+import { OpenWeatherMap } from "./Weather.jsx";
 
 export const HomePage = () => {
   const [serverStatuses, setServerStatuses] = useState({});
   const [serverStatusLoading, setserverStatusLoading] = useState(true);
   useEffect(() => {
+    getWidgets(
+      () => {
+        return;
+      },
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
     getServerStatuses(
       () => setserverStatusLoading(true),
       (result) => {
@@ -56,24 +67,24 @@ export const HomePage = () => {
         />
       </HStack>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={8}>
-        {serverStatusLoading ? (
-          // add 4 skeleton loaders using a for loop
-          Array.from({ length: 4 }, (_, i) => (
-            <ServerStatusSkeleton key={i} />
-          ))
-        ) : (
-          serverStatuses.map((serverState) => (
-            <ServerStatus
-              key={serverState.host.hostname}
-              serverState={serverState}
-            />
-          ))
-        )}
+        {serverStatusLoading
+          ? Array.from({ length: 4 }, (_, i) => (
+              <ServerStatusSkeleton key={i} />
+            ))
+          : serverStatuses.map((serverState) => (
+              <ServerStatus
+                key={serverState.host.hostname}
+                serverState={serverState}
+              />
+            ))}
       </SimpleGrid>
       <HStack>
         <Icon as={TbAppsFilled} w={6} h={6} marginRight={2} />
-        <Text fontSize="2xl">Apps</Text>
+        <Text fontSize="2xl">Widgets</Text>
       </HStack>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={8}>
+        <OpenWeatherMap />
+      </SimpleGrid>
     </VStack>
   );
 };
---
+++ frontend/src/components/Weather.jsx
@@ -0,0 +1,9 @@
+import { Card } from "@chakra-ui/react"
+
+export const OpenWeatherMap = () => {
+    return (
+        <Card variant="elevated" h={150}>
+            
+        </Card>
+    )
+}
