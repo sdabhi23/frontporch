06fb56f110a0853d1f54bab2dc122f9f49edeb3d
shrey <shrey.dabhi23@gmail.com>
2024-09-02 14:06:30 +0530

    [wip] weather details card

--- frontend/src/components/Weather.jsx
+++ frontend/src/components/Weather.jsx
@@ -1,5 +1,50 @@
-import { Card, Text, CardBody, Heading, CardHeader } from "@chakra-ui/react";
+import {
+  Card,
+  Text,
+  CardBody,
+  Heading,
+  CardHeader,
+  HStack,
+  Spacer,
+  Icon,
+} from "@chakra-ui/react";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiNightAltCloudy,
+  WiCloud,
+  WiCloudy,
+  WiShowers,
+  WiDayRain,
+  WiNightAltRain,
+  WiDayThunderstorm,
+  WiNightAltThunderstorm,
+  WiSnowflakeCold,
+  WiFog,
+} from "react-icons/wi";
 import PropTypes from "prop-types";
+
+const iconMap = {
+  "01d": WiDaySunny,
+  "01n": WiNightClear,
+  "02d": WiDayCloudy,
+  "02n": WiNightAltCloudy,
+  "03d": WiCloud,
+  "03n": WiCloud,
+  "04d": WiCloudy,
+  "04n": WiCloudy,
+  "09d": WiShowers,
+  "09n": WiShowers,
+  "10d": WiDayRain,
+  "10n": WiNightAltRain,
+  "11d": WiDayThunderstorm,
+  "11n": WiNightAltThunderstorm,
+  "13d": WiSnowflakeCold,
+  "13n": WiSnowflakeCold,
+  "50d": WiFog,
+  "50n": WiFog,
+};
 
 export const OpenWeatherMap = ({ data }) => {
   console.log(data);
@@ -7,9 +52,18 @@ export const OpenWeatherMap = ({ data }) => {
   return (
     <Card variant="elevated" h={150}>
       <CardHeader>
-        <Heading fontSize="lg">
-          {data.city_name}, {data.country}
-        </Heading>
+        <HStack>
+          <Icon as={iconMap[data.weather_0_icon]} h={10} w={10} />
+          <Spacer />
+          <Heading fontSize="lg">
+            {data.city_name}, {data.country}
+          </Heading>
+        </HStack>
+        <HStack>
+          <Spacer/>
+          <Text fontSize="m">{data.temp_actual}</Text>
+          <Text fontSize="xs">Feels like {data.temp_feels_like}</Text>
+        </HStack>
       </CardHeader>
       <CardBody></CardBody>
     </Card>
