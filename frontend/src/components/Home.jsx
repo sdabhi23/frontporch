11207fa6038938c97266d5d86f30fa0bc8e3e2d6
Shrey <shrey.dabhi23@gmail.com>
2024-09-03 13:16:21 +0530

    OpenWeatherMap Widget (#4)
    * setup api for weather widget
    
    * setup basic frontend skeleton
    
    * [wip] wireup frontend
    
    * lint
    
    * [wip] weather details card
    
    * support more os / system combos
    
    * backend changes
    
    * frontend changes

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,24 +6,60 @@ import {
   Spacer,
   Text,
   VStack,
+  Card,
+  CardBody,
+  SkeletonCircle,
+  SkeletonText,
 } from "@chakra-ui/react";
 import { FaRotate, FaServer } from "react-icons/fa6";
 import { TbAppsFilled } from "react-icons/tb";
 
 import { useEffect, useState } from "react";
-import { ServerStatus, ServerStatusSkeleton } from "./Server.jsx";
+import { getServerStatuses, getWidgets } from "../utils/apis.js";
+import { ServerStatus } from "./Server.jsx";
+import { OpenWeatherMap } from "./Weather.jsx";
 
-import { getServerStatuses } from "../utils/apis.js";
+const CardSkeleton = () => {
+  return (
+    <Card variant="elevated" h={150}>
+      <CardBody>
+        <HStack mb={6}>
+          <SkeletonCircle h={5} w={5} />{" "}
+          <SkeletonText noOfLines={1} w="100px" />
+          <Spacer />
+          <SkeletonCircle h={5} w={5} />
+          <SkeletonCircle h={5} w={5} />
+        </HStack>
+        <SkeletonText noOfLines={4} />
+      </CardBody>
+    </Card>
+  );
+};
 
 export const HomePage = () => {
   const [serverStatuses, setServerStatuses] = useState({});
-  const [serverStatusLoading, setserverStatusLoading] = useState(true);
+  const [serverStatusLoading, setServerStatusLoading] = useState(true);
+
+  const [widgetData, setWidgetData] = useState({});
+  const [widgetDataLoading, setWidgetDataLoading] = useState(true);
+
   useEffect(() => {
+    getWidgets(
+      () => setWidgetDataLoading(true),
+      (result) => {
+        setWidgetData(result);
+        setWidgetDataLoading(false);
+      },
+      (error) => {
+        console.error(error);
+        setWidgetDataLoading(false);
+      }
+    );
     getServerStatuses(
-      () => setserverStatusLoading(true),
+      () => setServerStatusLoading(true),
       (result) => {
         setServerStatuses(result);
-        setserverStatusLoading(false);
+        setServerStatusLoading(false);
       },
       (error) => {
         console.error(error);
@@ -42,10 +78,10 @@ export const HomePage = () => {
           icon={<FaRotate />}
           onClick={() => {
             getServerStatuses(
-              () => setserverStatusLoading(true),
+              () => setServerStatusLoading(true),
               (result) => {
                 setServerStatuses(result);
-                setserverStatusLoading(false);
+                setServerStatusLoading(false);
               },
               (error) => {
                 console.error(error);
@@ -56,24 +92,31 @@ export const HomePage = () => {
         />
       </HStack>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={8}>
-        {serverStatusLoading ? (
-          // add 4 skeleton loaders using a for loop
-          Array.from({ length: 4 }, (_, i) => (
-            <ServerStatusSkeleton key={i} />
-          ))
-        ) : (
-          serverStatuses.map((serverState) => (
-            <ServerStatus
-              key={serverState.host.hostname}
-              serverState={serverState}
-            />
-          ))
-        )}
+        {serverStatusLoading
+          ? Array.from({ length: 4 }, (_, i) => <CardSkeleton key={i} />)
+          : serverStatuses.map((serverState) => (
+              <ServerStatus
+                key={serverState.host.hostname}
+                serverState={serverState}
+              />
+            ))}
       </SimpleGrid>
       <HStack>
         <Icon as={TbAppsFilled} w={6} h={6} marginRight={2} />
-        <Text fontSize="2xl">Apps</Text>
+        <Text fontSize="2xl">Widgets</Text>
       </HStack>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={8}>
+        {widgetDataLoading
+          ? Array.from({ length: 4 }, (_, i) => <CardSkeleton key={i} />)
+          : widgetData.map((widget) => {
+              switch (widget.type) {
+                case "open_weather_map":
+                  return <OpenWeatherMap data={widget} />;
+                default:
+                  console.log("Value is something else");
+              }
+            })}
+      </SimpleGrid>
     </VStack>
   );
 };
